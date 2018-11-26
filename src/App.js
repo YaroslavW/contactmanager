e9ad1632736ac92b00f747f00dd0986e65226190
YaroslavW <doctor355@yahoo.com>
2018-11-26 18:48:14 +0300

    components bootstrap navbar and bootstrap configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-
+import {Grid, Row, Col} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        My App
+        <Header />
+        <Grid>
+          <Row>
+            <Col xs={12} md={12} lg={12}>
+              My App
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
